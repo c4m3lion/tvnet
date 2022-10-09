b4b1f6e732a7c258e60c4e536086be496e94a391
taanasaa <said.naghiyev@swisscom.com>
2022-10-09 21:17:23 +0300

    Oriontation fix

--- lib/pages/video/video_page.dart
+++ lib/pages/video/video_page.dart
@@ -1,4 +1,3 @@
-import 'package:better_player/better_player.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
@@ -203,6 +202,7 @@ class _VideoPageState extends State<VideoPage> {
 
   Widget bildMainFrame() {
     return Stack(
+      alignment: Alignment.center,
       children: [
         AspectRatio(
           aspectRatio: 16 / 9,
